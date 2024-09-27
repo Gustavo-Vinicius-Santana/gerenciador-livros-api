7455d302f3c68364b00a085b4e0beb1634bcb88d
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-09-26 22:02:54 -0300

    [feature] criando service do usuario

---
+++ app/Services/UserService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Hash;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAllUsers()
+    {
+        return $this->userRepository->getAll();
+    }
+
+    public function getUserById($id)
+    {
+        $user = $this->userRepository->findById($id);
+
+        if (!$user) {
+            return ['message' => 'esse usuario não existe'];
+        }
+
+        return $user;
+    }
+
+    public function createUser(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        return $this->userRepository->create($data);
+    }
+
+    public function updateUser($id, array $data)
+    {
+        $user = $this->getUserById($id);
+
+        if (isset($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+
+        return $this->userRepository->update($user, $data);
+    }
+
+    public function deleteUser($id)
+    {
+        $user = $this->getUserById($id);
+        return $this->userRepository->delete($user);
+    }
+}
