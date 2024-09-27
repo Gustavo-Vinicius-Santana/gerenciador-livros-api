19028d8a92769d22e34d44287128e309a0bbccef
Gustavo_Vinicius_SX <gus.tec.dev@gmail.com>
2024-09-26 22:00:06 -0300

    [feature] Gate de requisição

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('manage-profile', function (User $user, User $profile) {
+            return $user->id === $profile->id;
+        });
     }
 }
